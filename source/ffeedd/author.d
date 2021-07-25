0e94a91db0d4bc1c4587dbd4a67cbca2a2d68336
Ryhon <ryhon0@gmail.com>
2021-07-25 16:57:38 +0200

    Add Author(string)
    Used for converting strings ("Name <email@something.com>") to Author using to!Author or using a constructor

--- source/ffeedd/author.d
+++ source/ffeedd/author.d
@@ -1,7 +1,19 @@
 module ffeedd.author;
 
+import std.regex;
+
 struct Author
 {
+	/// Create author from a string. ("Name <email@something.com>")
+	this(string s)
+	{
+		auto authorReg = ctRegex!r"^(.*) <(.*@.*\..*)>$";
+		auto m = match(s, authorReg);
+
+		name = m.captures[1];
+		email = m.captures[2];
+	}
+
 	string name;
 	string email;
 }
